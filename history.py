911132594ddc6e4ae05c402ddc45b2d1a1f54c9f
Toni <toni.treichel@tt-digital.de>
2021-01-09 13:58:52 +0100

    Add files via upload

---
+++ history.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+import mplfinance as mpf
+from datetime import date
+
+stock = 'AAPL'
+today = date.today()
+period = '6mo' # 1mo, 1y,...
+
+df = pd.DataFrame(data)
+df.to_csv(str(today) + '_' + stock + '.csv')
+
+data = yf.download(stock, period=period)
+mpf.plot(data,type='line',mav=(10,20),volume=True,show_nontrading=True)
+
