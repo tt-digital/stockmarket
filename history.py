4a05990315da927df32080d1470c8272d32d7ecd
Toni <toni.treichel@tt-digital.de>
2021-01-09 14:11:10 +0100

    Update history.py

--- history.py
+++ history.py
@@ -8,7 +8,7 @@ today = date.today()
 period = '6mo' # 1mo, 1y,...
 
 data = yf.download(stock, period=period)
-mpf.plot(data,type='line',mav=(10,20),volume=True,show_nontrading=True)
+mpf.plot(data,type='line',mav=(10,20),volume=True,show_nontrading=True,title=stock + ' Price over ' + period)
 
 df = pd.DataFrame(data)
 df.to_csv(str(today) + '_' + stock + '.csv')
